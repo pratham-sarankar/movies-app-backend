b92d075976164290f6544e21c1e0ebb72cb65066
Pratham Sarankar <pratham@sarankar.com>
2025-08-07 19:51:50 +0530

    Add fetch movie API

--- src/controllers/movie_controller.js
+++ src/controllers/movie_controller.js
@@ -1,5 +1,22 @@
+import Movie from "../models/movie.js";
+
 export async function getMovies(req, res) {
+  const n = req.query.n || 2;
+  const q = req.query.q || "";
+  const page = req.query.page || 1;
+  const movies = await Movie.find({
+    $and: [
+      {
+        title: { $regex: q, $options: "i" },      
+      },
+      {
+        plot: { $regex: q, $options: "i" },
+      },
+    ],
+  })
+    .limit(n)
+    .skip(n * (page - 1));
   res.json({
-    movies: [],
+    movies: movies,
   });
 }
